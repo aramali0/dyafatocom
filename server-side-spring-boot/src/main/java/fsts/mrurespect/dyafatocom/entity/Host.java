259456e2d528052b0d6d57d5d504440c556f1a60
ARAMALI Mohammed <mouhammedaramali@gmail.com>
2024-02-27 22:56:15 +0100

    connect backend with front end tourist page

--- server-side-spring-boot/src/main/java/fsts/mrurespect/dyafatocom/entity/Host.java
+++ server-side-spring-boot/src/main/java/fsts/mrurespect/dyafatocom/entity/Host.java
@@ -3,22 +3,20 @@ package fsts.mrurespect.dyafatocom.entity;
 import fsts.mrurespect.dyafatocom.Enums.Service;
 import fsts.mrurespect.dyafatocom.entity.messagerie.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Table(name = "user")
 @Entity
 @DiscriminatorValue("HOST")
-@Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Host extends User {
 
     @Column
-    private String cin ;
+    private String cin;
     @Column
     private String description ;
     @OneToMany(cascade = CascadeType.ALL)
@@ -28,5 +26,6 @@ public class Host extends User {
     @Enumerated(EnumType.STRING)
     private List<Service> services ;
     private  int rating ;
+
 
 }
